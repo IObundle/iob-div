78d3470185b36579d6f48e0dc61250c2d32b8f59
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-01-09 18:13:51 +0000

    simplify subtract-shift unsigned divider, remove asic folder

--- hardware/simulation/testbench/div_subshift_tb.v
+++ hardware/simulation/testbench/div_subshift_tb.v
@@ -4,28 +4,23 @@ module div_subshift_tb;
 
    parameter clk_frequency = `CLK_FREQ;
    parameter clk_period = 1e9/clk_frequency; //ns
-   parameter DATA_W = 16; //data gen is limited to 31 bits
-   parameter TEST_SZ = 100;
+   parameter DATA_W = 8;
+   parameter TEST_SZ = 1000;
 
-   reg clk;
-   reg en;
+   reg clk = 0;
+   reg rst = 0;
+   reg start = 0;
    wire done;
 
    //data
    reg [DATA_W-1:0]  dividend [0:TEST_SZ-1];
    reg [DATA_W-1:0]  divisor [0:TEST_SZ-1];
-   reg signed [DATA_W-1:0] dividend_s [0:TEST_SZ-1];
-   reg signed [DATA_W-1:0] divisor_s [0:TEST_SZ-1];
    reg [DATA_W-1:0]        quotient [0:TEST_SZ-1];
    reg [DATA_W-1:0]        remainder [0:TEST_SZ-1];
-   reg signed [DATA_W-1:0] quotient_s [0:TEST_SZ-1];
-   reg signed [DATA_W-1:0] remainder_s [0:TEST_SZ-1];
 
    //core outputs
    wire [DATA_W-1:0]        quotient_out;
    wire [DATA_W-1:0]        remainder_out;
-   wire signed [DATA_W-1:0] quotient_s_out;
-   wire signed [DATA_W-1:0] remainder_s_out;
    
    integer           i;
 
@@ -37,31 +32,30 @@ module div_subshift_tb;
       $dumpvars();
 `endif
 
-      clk = 1;
-      en = 0;
-
       // generate test data
       for (i=0; i < TEST_SZ; i=i+1) begin
 	 dividend[i] = $random;
 	 divisor[i] = $random;
 	 quotient[i] = dividend[i] / divisor[i];
 	 remainder[i] = dividend[i] % divisor[i];
-         dividend_s[i] = $random%(DATA_W)-2**(DATA_W-1);
-	 divisor_s[i] = $random%(DATA_W)-2**(DATA_W-1);
-	 quotient_s[i] = dividend_s[i] / divisor_s[i];
-	 remainder_s[i] = dividend_s[i] % divisor_s[i];         
       end
+            
+      //reset pulse
+      #100 rst = 1;
+      @(posedge clk) #1 rst = 0;
       
-      
-      //copmpute divisions
+      //compute divisions
       for (i=0; i < TEST_SZ; i=i+1) begin
-         @(posedge clk) #1 en = 1;
+         //pulse start
+         @(posedge clk) #1 start = 1;
+         @(posedge clk) #1 start = 0;
+
+         //wait for done
          @(posedge done) #1;
+
+         //verify results
          if(quotient_out != quotient[i] || remainder_out != remainder[i])
            $display ("%d / %d = %d with rem %d but got %d with rem %d", dividend[i], divisor[i], quotient[i], remainder[i], quotient_out, remainder_out);
-         else if (quotient_s_out != quotient_s[i] || remainder_s_out != remainder_s[i])
-           $display ("%d / %d = %d with rem %d but got %d with rem %d", dividend_s[i], divisor_s[i], quotient[i], remainder[i], quotient_s_out, remainder_s_out);
-         @(posedge clk) #1 en = 0;
       end
 
       $finish;
@@ -79,30 +73,14 @@ module div_subshift_tb;
    uut 
      (
       .clk(clk),
-      .en(en),
+      .rst(rst),
+      .start(start),
       .done(done),
-      .sign(1'b0),
+
       .dividend(dividend[i]),
       .divisor(divisor[i]),
       .quotient(quotient_out),
       .remainder(remainder_out)
-      );
-
-   //instantiate signed divider
-   div_subshift 
-     # (
-        .DATA_W(DATA_W)
-        )
-   uut_s 
-     (
-      .clk(clk),
-      .en(en),
-      .done(done),
-      .sign(1'b1),
-      .dividend(dividend_s[i]),
-      .divisor(divisor_s[i]),
-      .quotient(quotient_s_out),
-      .remainder(remainder_s_out)
       );
    
 endmodule
--- hardware/src/div_subshift.v
+++ hardware/src/div_subshift.v
@@ -8,7 +8,7 @@ module div_subshift
     input               clk,
     input               rst,
     input               start,
-    output reg          done,
+    output              done,
 
     input [DATA_W-1:0]  dividend,
     input [DATA_W-1:0]  divisor,
@@ -16,60 +16,66 @@ module div_subshift
     output [DATA_W-1:0] remainder
     );
 
-   reg [2*DATA_W:0]     rq;
-   reg [DATA_W-1:0]     divisor_reg;
-   wire [DATA_W-1:0] subtraend = rq[2*DATA_W-2-:DATA_W];
-   reg [DATA_W:0]           tmp;
+   //dividend/quotient/remainder register
+   reg [2*DATA_W:0]     dqr_reg, dqr_nxt;
+   always @(posedge clk, posedge rst) if(rst) dqr_reg <= 1'b1; else dqr_reg <= dqr_nxt;
+
+   //divisor register
+   reg [DATA_W-1:0]     divisor_reg, divisor_nxt;
+   always @(posedge clk, posedge rst) if(rst) divisor_reg <= 1'b1; else divisor_reg <= divisor_nxt;
+   
+   wire [DATA_W-1:0]    subtraend = dqr_reg[2*DATA_W-2-:DATA_W];
+   reg [DATA_W:0]       tmp;
 
    //output quotient and remainder
-   assign quotient = rq[DATA_W-1:0];
-   assign remainder = rq[2*DATA_W-1:DATA_W];
+   assign quotient = dqr_reg[DATA_W-1:0];
+   assign remainder = dqr_reg[2*DATA_W-1:DATA_W];
 
-
-   reg [$clog2(DATA_W+5):0] pc;  //program counter
-
+   //done register
+   reg                  done_reg, done_nxt;
+   always @(posedge clk, posedge rst) if(rst) done_reg <= 1'b1; else done_reg <= done_nxt;
+   assign done = done_reg;
+   
+                        
    //
    //PROGRAM
    //
-   
-   always @(posedge clk, posedge rst) begin
-      if(rst) begin
-         pc <= 1'b0;
-         rq <= 1'b0;
-         done <= 1'b1;
-         divisor_reg <= 1'b0;
-     end else begin
-         pc <= pc+1'b1;
-         
-         case (pc)
-	   0: begin //wait start
-              divisor_reg <= 1'b0;
-              rq <= 1'b0;
-              if(!start) begin
-                 pc <= pc;
-                 done <= 1'b0;
-              end
-	   end // case: 0
 
-           1:  begin //load operands
-              divisor_reg <= divisor;
-              rq[DATA_W-1:0] <= dividend;
-           end
-           
-	   DATA_W+2: begin  //finish 
- 	      done <= 1'b1;
-              pc <= 1'b0;
+   reg [$clog2(DATA_W+1):0] pc, pc_nxt;  //program counter
+   always @(posedge clk, posedge rst) if(rst) pc <= 1'b0; else pc <= pc_nxt;
+
+   always @* begin
+      pc_nxt = pc+1'b1;
+      dqr_nxt = dqr_reg;
+      done_nxt = done_reg;
+      divisor_nxt = divisor;
+      
+      case (pc)
+	0: begin //wait for start, load operands and deassert done
+           divisor_nxt = 1'b0;
+           dqr_nxt = 1'b0;
+           if(!start) begin
+              pc_nxt = pc;
+           end else begin
+              done_nxt = 1'b0;
+              divisor_nxt = divisor;
+              dqr_nxt[DATA_W-1:0] = dividend;
 	   end
+        end
 	   
-	   default: begin //shift and subtract
-	      tmp = {1'b0, subtraend} - {1'b0, divisor_reg};
-              if(~tmp[DATA_W])
-                rq <= {tmp, rq[DATA_W-2 : 0], 1'b1};
-              else 
-                rq <= {rq[2*DATA_W-2 : 0], 1'b0};
+	default: begin //shift and subtract
+	   tmp = {1'b0, subtraend} - {1'b0, divisor_reg};
+           if(~tmp[DATA_W])
+             dqr_nxt = {tmp, dqr_reg[DATA_W-2 : 0], 1'b1};
+           else 
+             dqr_nxt = {dqr_reg[2*DATA_W-2 : 0], 1'b0};
+
+           if(pc == DATA_W) begin
+              done_nxt = 1'b1;
+              pc_nxt = 1'b0;
            end
-         endcase // case (pc)
-      end
-   end // always @ *
+        end
+      endcase // case (pc)
+   end
    
 endmodule
